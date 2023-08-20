598a6ceb8a5c58074beabac2d3effa3fa6e5c227
michael-s-b <michelsilva098@gmail.com>
2023-08-20 09:33:18 -0300

    demo - update data from db

--- src/App.java
+++ src/App.java
@@ -1,39 +1,21 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.text.SimpleDateFormat;
-
 import db.DB;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection conn = null;
         PreparedStatement st = null;
         try {
             conn = DB.getConnection();
-            st = conn.prepareStatement(
-                    "INSERT INTO seller" +
-                            "(Name, Email, BirthDate, BaseSalary, DepartmentId)" +
-                            "VALUES" +
-                            "(?,?,?,?,?)",
-                    Statement.RETURN_GENERATED_KEYS);
-            st.setString(1, "Carl Purple");
-            st.setString(2, "carl@gmail.com");
-            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-            st.setDouble(4, 3000.0);
-            st.setInt(5, 4);
-            Integer rowsAffected = st.executeUpdate();
-            if (rowsAffected > 0) {
-                ResultSet rs = st.getGeneratedKeys();
-                while (rs.next()) {
-                    int id = rs.getInt(1);
-                    System.out.println("Done! Id = " + id);
-                }
-            } else {
-                System.out.println("No rows affected!");
-            }
+            st = conn.prepareStatement("update seller " +
+                    "set baseSalary = baseSalary + ? " +
+                    "where " +
+                    "(DepartmentId = ?)");
+            st.setDouble(1, 200.0);
+            st.setInt(2, 2);
+            int rowsAffected = st.executeUpdate();
+            System.out.println("Done! Rows affected: " + rowsAffected);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
